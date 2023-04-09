f10084b5a2823e3bca92d8f42ed41ed771fc82df
warriors230 <baironduvan1998@hotmail.com>
2023-04-08 22:53:51 -0500

    Commit inicial

---
+++ src/main/java/com/warriors/easyStock/EasyStockApplication.java
@@ -0,0 +1,13 @@
+package com.warriors.easyStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warriors/easyStock/EasyStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warriors.easyStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
